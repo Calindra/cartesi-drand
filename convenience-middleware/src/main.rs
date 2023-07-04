e47b2b916d0921085c82ff66afe9bc6770b8a04a
Bruno Ochotorena <hackerhilt@gmail.com>
2023-07-04 20:20:57 -0300

    feat: add comunication with rollups

--- convenience-middleware/src/main.rs
+++ convenience-middleware/src/main.rs
@@ -1,6 +1,8 @@
 use actix_web::{get, post, web, App, HttpServer, Responder};
+use dotenv::dotenv;
+use json::object;
 use serde::Deserialize;
-use std::sync::Mutex;
+use std::{env, sync::Mutex};
 
 #[derive(Deserialize, Clone)]
 struct Item {
@@ -75,11 +77,79 @@ async fn consume_buffer(ctx: web::Data<AppState>) -> impl Responder {
     result
 }
 
+async fn rollup() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Starting rollup");
+
+    let client = hyper::Client::new();
+    let server_addr = env::var("ROLLUP_HTTP_SERVER_URL")?;
+
+    let mut status = "accept";
+    loop {
+        println!("Sending finish");
+        let response = object! {"status" => status.clone()};
+        let request = hyper::Request::builder()
+            .method(hyper::Method::POST)
+            .header(hyper::header::CONTENT_TYPE, "application/json")
+            .uri(format!("{}/finish", &server_addr))
+            .body(hyper::Body::from(response.dump()))?;
+        let response = client.request(request).await?;
+        println!("Received finish status {}", response.status());
+
+        if response.status() == hyper::StatusCode::ACCEPTED {
+            println!("No pending rollup request, trying again");
+        } else {
+            let body = hyper::body::to_bytes(response).await?;
+            let utf = std::str::from_utf8(&body)?;
+            let req = json::parse(utf)?;
+
+            let request_type = req["request_type"]
+                .as_str()
+                .ok_or("request_type is not a string")?;
+            status = match request_type {
+                "advance_state" => handle_advance(&client, &server_addr[..], req).await?,
+                "inspect_state" => handle_inspect(&client, &server_addr[..], req).await?,
+                &_ => {
+                    eprintln!("Unknown request type");
+                    "reject"
+                }
+            };
+        }
+    }
+}
+
+async fn handle_inspect(
+    client: &hyper::Client<hyper::client::HttpConnector>,
+    server_addr: &str,
+    req: json::JsonValue,
+) -> Result<&'static str, Box<dyn std::error::Error>> {
+    println!("Handling inspect");
+
+    Ok("accept")
+}
+
+async fn handle_advance(
+    client: &hyper::Client<hyper::client::HttpConnector>,
+    server_addr: &str,
+    req: json::JsonValue,
+) -> Result<&'static str, Box<dyn std::error::Error>> {
+    println!("Handling advance");
+
+    Ok("accept")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
     let app_state = web::Data::new(AppState {
         input_buffer_manager: InputBufferManager::new(),
     });
+
+    tokio::spawn(async move {
+        rollup().await;
+    });
+
+    println!("Starting server");
 
     HttpServer::new(move || {
         App::new()
