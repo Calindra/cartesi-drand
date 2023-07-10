8bfc5aea91bbb5721a3b28b3c044e2a8900a1ec6
Bruno Ochotorena <hackerhilt@gmail.com>
2023-07-10 17:55:23 -0300

    feat: add some code

--- dapp-contract-blackjack/src/main.rs
+++ dapp-contract-blackjack/src/main.rs
@@ -1,3 +1,49 @@
 fn main() {
     println!("Hello, world!");
 }
+
+stuct Card {
+    naipe: String,
+    valor: String,
+}
+
+
+async fn rollup(sender: Sender<Item>) -> Result<(), Box<dyn std::error::Error>> {
+    println!("Starting middleware sender");
+
+    let client = hyper::Client::new();
+    let server_addr = env::var("MIDDLEWARE_HTTP_SERVER_URL")?;
+
+    let mut status = "accept";
+    loop {
+        println!("Sending finish");
+        let response = object! {"status" => status.clone()};
+        let request = hyper::Request::builder()
+            .method(hyper::Method::POST)
+            .header(hyper::header::CONTENT_TYPE, "application/json")
+            .uri(format!("{}/finish", &server_addr))
+            .body(hyper::Body::from(response.dump()))?;
+        let response = client.request(request).await?;
+        println!("Received finish status {}", response.status());
+
+        if response.status() == hyper::StatusCode::ACCEPTED {
+            println!("No pending rollup request, trying again");
+        } else {
+            let body = hyper::body::to_bytes(response).await?;
+            let utf = std::str::from_utf8(&body)?;
+            let req = json::parse(utf)?;
+
+            let request_type = req["request_type"]
+                .as_str()
+                .ok_or("request_type is not a string")?;
+            status = match request_type {
+                "advance_state" => handle_advance(&client, &server_addr[..], req, &sender).await?,
+                "inspect_state" => handle_inspect(&client, &server_addr[..], req).await?,
+                &_ => {
+                    eprintln!("Unknown request type");
+                    "reject"
+                }
+            };
+        }
+    }
+}
