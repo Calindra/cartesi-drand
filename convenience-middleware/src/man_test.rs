900a5579b77c083680ba584f82a9adb86e2ccf58
Bruno Ochotorena <hackerhilt@gmail.com>
2023-07-17 10:50:22 -0300

    test: add test

--- convenience-middleware/src/man_test.rs
+++ convenience-middleware/src/man_test.rs
@@ -1,11 +1,16 @@
 #[cfg(test)]
 mod tests {
     // use super::*;
-    use crate::router::routes::{self};
+    use crate::{
+        is_drand_beacon,
+        models::models::Item,
+        router::routes::{self},
+    };
     use actix_web::{
         http::{self, header::ContentType},
         test,
     };
+    use serde_json::json;
 
     #[actix_web::test]
     async fn test_index_ok() {
@@ -15,6 +20,33 @@ mod tests {
 
         let resp = routes::hello().await;
         assert_eq!(resp.status(), http::StatusCode::OK);
+    }
+
+    #[actix_web::test]
+    async fn test_main_beacon() {
+        let beacon = json!({
+            "beacon": {
+                "round": 3828300,
+                "randomness": "7ff726d290836da706126ada89f7e99295c672d6768ec8e035fd3de5f3f35cd9",
+                "signature": "ab85c071a4addb83589d0ecf5e2389f7054e4c34e0cbca65c11abc30761f29a0d338d0d307e6ebcb03d86f781bc202ee"
+            },
+        });
+        let beacon = beacon.to_string();
+        let beacon = hex::encode(beacon);
+        let beacon = format!("0x{}", beacon);
+
+        let object = json!({
+            "data": {
+                "payload": beacon,
+            }
+        });
+
+        let item = Item {
+            request: object.to_string(),
+        };
+
+        let resp = is_drand_beacon(&item);
+        assert_eq!(resp, true);
     }
 
     // #[actix_web::test]
