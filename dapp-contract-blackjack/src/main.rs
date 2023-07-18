528ae172dda1849a8224dd2949721e1a8f278065
Bruno Ochotorena <hackerhilt@gmail.com>
2023-07-18 17:15:15 -0300

    feat: more changes

--- dapp-contract-blackjack/src/main.rs
+++ dapp-contract-blackjack/src/main.rs
@@ -18,6 +18,7 @@ struct Player {
     name: String,
     hand: Vec<Card>,
     has_ace: bool,
+    bet: u32,
 }
 
 impl Player {
@@ -26,7 +27,45 @@ impl Player {
             name,
             hand: Vec::new(),
             has_ace: false,
+            bet: 0,
         }
+    }
+
+    /**
+     * Take a card from the deck and add it to the player's hand.
+     */
+    fn hit() -> Card {
+        let card = Card::Ace;
+
+        card
+    }
+
+    /**
+     * Add the value of the card to the player's hand.
+     */
+    fn stand() {
+        todo!();
+    }
+
+    /**
+     * Double the bet and take one more card.
+     */
+    fn double_down() {
+        todo!();
+    }
+
+    /**
+     * Split the hand into two separate hands.
+     */
+    fn split() {
+        todo!();
+    }
+
+    /**
+     * Give up the hand and lose half of the bet.
+     */
+    fn surrender() {
+        todo!();
     }
 }
 
@@ -45,6 +84,7 @@ impl Default for Deck {
 struct Table {
     players: Vec<Player>,
     deck: Deck,
+    bet: u32,
 }
 
 impl Table {
@@ -52,16 +92,39 @@ impl Table {
         Table {
             players: Vec::new(),
             deck: Deck::default(),
+            bet: 0,
         }
     }
 
     fn player_join(&mut self, player: Player) {
-        self.players.push(player);
+        self.players.push(player)
+    }
+
+    fn round_start(self) -> Game {
+        Game::new(self)
     }
 }
 
-fn create_table() {
-    let table = Table::new();
+struct Game {
+    table: Table,
+}
+
+impl Game {
+    fn new(table: Table) -> Game {
+        Game { table }
+    }
+}
+
+fn start_game(nth_players: u8) {
+    let mut table = Table::new();
+
+    for i in 0..nth_players {
+        let player = Player::new(format!("Player {}", i));
+
+        table.player_join(player);
+    }
+
+    let game = table.round_start();
 }
 
 fn main() {
