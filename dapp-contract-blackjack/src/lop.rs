b45022e20b855bebc1def00920cddb6c1deeeb68
Bruno Ochotorena <hackerhilt@gmail.com>
2023-07-24 22:12:39 -0300

    feat: add loop

---
+++ dapp-contract-blackjack/src/lop.rs
@@ -0,0 +1,73 @@
+pub mod rollup {
+    use hyper::{
+        body::to_bytes, client::HttpConnector, header, Body, Client, Method, Request, StatusCode,
+    };
+    use serde_json::{from_str, json, Value};
+    use std::{env, error::Error, str::from_utf8};
+
+    pub async fn rollup() -> Result<(), Box<dyn Error>> {
+        println!("Starting loop...");
+
+        let client = Client::new();
+        let server_addr = env::var("MIDDLEWARE_HTTP_SERVER_URL")?;
+
+        let mut status = "accept";
+        loop {
+            println!("Sending finish");
+            let response = json!({ "status": status.clone() });
+            let request = Request::builder()
+                .method(Method::POST)
+                .header(header::CONTENT_TYPE, "application/json")
+                .uri(format!("{}/finish", &server_addr))
+                .body(Body::from(response.to_string()))?;
+            let response = client.request(request).await?;
+            let status_response = response.status();
+            println!("Receive finish status {}", &status_response);
+
+            if status_response == StatusCode::ACCEPTED {
+                println!("No pending rollup request, trying again");
+            } else {
+                let body = to_bytes(response).await?;
+                let body = from_utf8(&body)?;
+                let body = from_str::<Value>(body)?;
+
+                let request_type = body["request_type"]
+                    .as_str()
+                    .ok_or("request_type is not a string")?;
+
+                status = match request_type {
+                    "advance_state" => handle_advance(&client, &server_addr[..], body).await?,
+                    "inspect_state" => handle_inspect(&client, &server_addr[..], body).await?,
+                    &_ => {
+                        eprintln!("Unknown request type");
+                        "reject"
+                    }
+                }
+            }
+        }
+    }
+
+    async fn handle_inspect(
+        client: &Client<HttpConnector>,
+        server_addr: &str,
+        body: Value,
+    ) -> Result<&'static str, Box<dyn Error>> {
+        println!("Handling inspect");
+
+        println!("body {:}", &body);
+
+        Ok("accept")
+    }
+
+    async fn handle_advance(
+        client: &Client<HttpConnector>,
+        server_addr: &str,
+        body: Value,
+    ) -> Result<&'static str, Box<dyn Error>> {
+        println!("Handling inspect");
+
+        println!("body {:}", &body);
+
+        Ok("accept")
+    }
+}
--- dapp-contract-blackjack/src/main.rs
+++ dapp-contract-blackjack/src/main.rs
@@ -1,16 +1,36 @@
 use std::{
+    collections::{BTreeMap, BinaryHeap},
     fmt::{self, Display},
     sync::Arc,
 };
 
+mod lop;
 mod main_test;
 mod models;
 mod util;
-// mod loop;
 
+use dotenv::dotenv;
+
+use crate::lop::rollup::rollup;
 use crate::models::card::card::{Card, Rank, Suit};
 use crate::models::player::player::{Credit, Hand, Player, PlayerBet};
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    // dotenv().unwrap();
+
+    println!("Hello world");
+
+    let mut map = BTreeMap::new();
+
+    map.insert(2, "b");
+    map.insert(1, "a");
+    map.insert(3, "c");
+
+    let s = map.range(1..).next().unwrap();
+
+
+    assert_eq!(*s.0, 1);
+
+    // rollup().await.unwrap();
 }
