598f866e0240fbfc40349ab4ef3b43af8f3f8765
Fabio Oshiro <fabio.oshiro@calindra.com.br>
2023-08-17 14:39:04 -0300

    refactor: moving tests to tests folder

--- dapp-contract-blackjack/src/models/game.rs
+++ dapp-contract-blackjack/src/models/game.rs
@@ -3,8 +3,7 @@ pub mod game {
         models::{
             card::card::Deck,
             player::player::{Player, PlayerHand},
-        },
-        util::random::generate_id,
+        }, util::random::generate_id,
     };
     use serde_json::json;
     use std::{collections::HashMap, sync::Arc};
@@ -390,6 +389,13 @@ pub mod game {
                 .ok_or("Player not found.")
         }
 
+        #[cfg(test)]
+        pub fn change_points(&mut self, player_id: &str, points: u8) -> Result<(), &'static str> {
+            let hand = self.get_player_by_id_mut(player_id)?;
+            hand.points = points;
+            Ok(())
+        }
+
         pub fn get_player_by_id(&self, id: &str) -> Result<&PlayerHand, &'static str> {
             self.players_with_hand
                 .iter()
@@ -423,102 +429,6 @@ pub mod game {
             }
 
             winner
-        }
-    }
-}
-
-#[cfg(test)]
-mod game_tests {
-    use std::sync::Arc;
-
-    use crate::models::{game::game::Manager, player::player::Player};
-
-    #[tokio::test]
-    async fn get_winner_tests() {
-        let mut manager = Manager::new_with_games(1);
-
-        // Get ref for first game
-        let game = manager.first_game_available().unwrap();
-        let game_id = game.get_id().to_owned();
-
-        // Add players
-        let bob_address_owner = "f39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
-        let bob_address_encoded = bs58::encode(bob_address_owner).into_string();
-        let bob = Player::new(bob_address_encoded.clone(), "Bob".to_owned());
-        let bob = Arc::new(bob);
-        manager.add_player(bob.clone()).unwrap();
-        manager.player_join(game_id.to_owned(), bob).unwrap();
-
-        let alice_address = "70997970C51812dc3A010C7d01b50e0d17dc79C8";
-        let alice_address_encoded = bs58::encode(alice_address).into_string();
-        let alice = Player::new(alice_address_encoded.clone(), "Alice".to_owned());
-        let alice = Arc::new(alice);
-        manager.add_player(alice.clone()).unwrap();
-        manager.player_join(game_id.to_owned(), alice).unwrap();
-
-        // Get game and make owner
-        let game = manager.drop_game(game_id.to_owned()).unwrap();
-
-        // Generate table from game
-        let timestamp: u64 = 1691386341757;
-        let table = game.round_start(2, timestamp).unwrap();
-        // Add table to manager
-        manager.add_table(table);
-
-        {
-            let table = manager.get_table(&game_id).unwrap();
-            let bob_hand = table.find_player_by_id_mut(&bob_address_encoded).unwrap();
-            bob_hand.points = 20;
-
-            let alice_hand = table.find_player_by_id_mut(&alice_address_encoded).unwrap();
-            alice_hand.points = 21;
-
-            let winner = table.get_winner().await.unwrap();
-            assert_eq!("Alice", winner.name);
-        }
-        {
-            let table = manager.get_table(&game_id).unwrap();
-            let bob_hand = table.find_player_by_id_mut(&bob_address_encoded).unwrap();
-            bob_hand.points = 20;
-
-            let alice_hand = table.find_player_by_id_mut(&alice_address_encoded).unwrap();
-            alice_hand.points = 19;
-
-            let winner = table.get_winner().await.unwrap();
-            assert_eq!("Bob", winner.name);
-        }
-        {
-            let table = manager.get_table(&game_id).unwrap();
-            let bob_hand = table.find_player_by_id_mut(&bob_address_encoded).unwrap();
-            bob_hand.points = 20;
-
-            let alice_hand = table.find_player_by_id_mut(&alice_address_encoded).unwrap();
-            alice_hand.points = 20;
-
-            let winner = table.get_winner().await;
-            assert!(winner.is_none());
-        }
-        {
-            let table = manager.get_table(&game_id).unwrap();
-            let bob_hand = table.find_player_by_id_mut(&bob_address_encoded).unwrap();
-            bob_hand.points = 20;
-
-            let alice_hand = table.find_player_by_id_mut(&alice_address_encoded).unwrap();
-            alice_hand.points = 22;
-
-            let winner = table.get_winner().await.unwrap();
-            assert_eq!("Bob", winner.name);
-        }
-        {
-            let table = manager.get_table(&game_id).unwrap();
-            let bob_hand = table.find_player_by_id_mut(&bob_address_encoded).unwrap();
-            bob_hand.points = 22;
-
-            let alice_hand = table.find_player_by_id_mut(&alice_address_encoded).unwrap();
-            alice_hand.points = 22;
-
-            let winner = table.get_winner().await;
-            assert!(winner.is_none());
         }
     }
 }
