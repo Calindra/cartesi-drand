0efca35619b650a68ce80c02a1a1a599ec7de37e
Bruno Ochotorena <hackerhilt@gmail.com>
2023-09-19 21:20:07 -0300

    fix: fix connection

--- dapp-contract-blackjack/src/main.rs
+++ dapp-contract-blackjack/src/main.rs
@@ -5,7 +5,7 @@ mod rollups;
 mod util;
 
 use dotenv::dotenv;
-use rollups::rollup::{rollup, send_report,handle_request_action};
+use rollups::rollup::{handle_request_action, rollup, send_report};
 use serde_json::Value;
 use tokio::sync::{
     mpsc::{channel, Receiver, Sender},
@@ -13,236 +13,6 @@ use tokio::sync::{
 };
 
 use crate::models::game::game::Manager;
-
-// pub async fn handle_request_action(
-//     root: &Value,
-//     manager: Arc<Mutex<Manager>>,
-//     need_write: bool,
-// ) -> Result<Option<Value>, &'static str> {
-//     let payload = get_payload_from_root(root).ok_or("Invalid payload")?;
-//     let action = get_from_payload_action(&payload);
-
-//     println!("Action: {:}", action.as_deref().unwrap_or("None"));
-
-//     match action.as_deref() {
-//         Some("new_player") => {
-//             let input = payload.get("input").ok_or("Invalid field input")?;
-//             let player_name = check_fields_create_player(&input)?;
-
-//             let encoded_name = bs58::encode(&player_name).into_string();
-
-//             let metadata = get_address_metadata_from_root(root).ok_or("Invalid address")?;
-//             let address_owner = metadata.address.trim_start_matches("0x");
-//             let address_encoded = bs58::encode(address_owner).into_string();
-
-//             // Add player to manager
-//             let player = Player::new(address_encoded.clone(), player_name.to_string());
-//             let mut manager = manager.lock().await;
-//             let player = Arc::new(player);
-//             manager.add_player(player)?;
-
-//             // Persist player
-//             if need_write {
-//                 let address_owner_obj = json!({ "address": address_owner, "name": player_name });
-//                 let address_path = format!("./data/address/{}.json", address_encoded);
-
-//                 write_json(&address_path, &address_owner_obj)
-//                     .await
-//                     .or(Err("Could not write address"))?;
-
-//                 let player_path = format!("./data/names/{}.json", encoded_name);
-//                 let player = json!({ "name": encoded_name, "address": metadata.address });
-//                 write_json(&player_path, &player)
-//                     .await
-//                     .or(Err("Could not write player"))?;
-//             }
-
-//             let response = generate_message(json!({
-//                 "address": address_encoded,
-//                 "encoded_name": encoded_name,
-//                 "name": player_name,
-//             }));
-
-//             println!("Response: {:}", response);
-
-//             return Ok(Some(response));
-//         }
-//         Some("join_game") => {
-//             let input = payload.get("input").ok_or("Invalid field input")?;
-
-//             // Address
-//             let metadata = get_address_metadata_from_root(root).ok_or("Invalid address")?;
-//             let address_owner = metadata.address.trim_start_matches("0x");
-//             let address_encoded = bs58::encode(address_owner).into_string();
-
-//             let mut manager = manager.lock().await;
-//             let player = manager.get_player_ref(&address_encoded)?;
-
-//             // Parsing JSON
-//             let game_id = input
-//                 .get("game_id")
-//                 .ok_or("Invalid field game_id")?
-//                 .as_str()
-//                 .ok_or("Invalid game_id")?;
-
-//             manager.player_join(game_id, player.clone())?;
-//             println!("Player joined: name {} game_id {}", player.name, game_id);
-//         }
-//         Some("show_games") => {
-//             let manager = manager.lock().await;
-//             let games = manager.show_games_id_available();
-
-//             let response = generate_message(json!({
-//                 "games": games,
-//             }));
-
-//             println!("Response: {:}", response);
-
-//             return Ok(Some(response));
-//         }
-//         Some("start_game") => {
-//             let input = payload.get("input").ok_or("Invalid field input")?;
-//             let metadata = get_address_metadata_from_root(root).ok_or("Invalid address")?;
-//             // Parsing JSON
-//             let game_id = input
-//                 .get("game_id")
-//                 .ok_or("Invalid field game_id")?
-//                 .as_str()
-//                 .ok_or("Invalid game_id")?;
-
-//             let mut manager = manager.lock().await;
-
-//             // Get game and make owner
-//             let game = manager.drop_game(game_id)?;
-//             // Generate table from game
-//             let table = game.round_start(2, metadata.timestamp)?;
-//             // Add table to manager
-//             manager.add_table(table);
-//             println!("Game started: game_id {}", game_id);
-//         }
-//         Some("stop_game") => {
-//             let input = payload.get("input").ok_or("Invalid field input")?;
-
-//             // Parsing JSON
-//             let game_id = input
-//                 .get("game_id")
-//                 .ok_or("Invalid field game_id")?
-//                 .as_str()
-//                 .ok_or("Invalid game_id")?;
-
-//             let mut manager = manager.lock().await;
-
-//             manager.stop_game(game_id).await?;
-//         }
-//         Some("show_hands") => {
-//             let input = payload.get("input").ok_or("Invalid field input")?;
-
-//             // Parsing JSON
-//             let game_id = input
-//                 .get("game_id")
-//                 .ok_or("Invalid field game_id")?
-//                 .as_str()
-//                 .ok_or("Invalid game_id")?;
-
-//             let mut manager = manager.lock().await;
-
-//             let table = manager.get_table(game_id)?;
-//             let hands = table.generate_hands();
-//             let report = generate_message(hands);
-
-//             println!("Report: {:}", report);
-
-//             return Ok(Some(report));
-//         }
-//         Some("show_winner") => {
-//             let input = payload.get("input").ok_or("Invalid field input")?;
-
-//             // Parsing JSON
-//             let game_id = input
-//                 .get("game_id")
-//                 .ok_or("Invalid field game_id")?
-//                 .as_str()
-//                 .ok_or("Invalid game_id")?;
-
-//             let table_id = input
-//                 .get("table_id")
-//                 .ok_or("Invalid field table_id")?
-//                 .as_str()
-//                 .ok_or("Invalid string table_id")?;
-
-//             let manager = manager.lock().await;
-
-//             println!(
-//                 "Finding score by table_id {} and game_id {} ...",
-//                 table_id, game_id
-//             );
-//             let scoreboard = manager
-//                 .get_scoreboard(table_id, game_id)
-//                 .ok_or("Scoreboard not found searching by table_id")?;
-
-//             let response = generate_message(scoreboard.to_json());
-
-//             let response = generate_message(json!(response));
-
-//             println!("Response: {:}", response);
-
-//             return Ok(Some(response));
-//         }
-//         Some("hit") => {
-//             // Address
-//             let metadata = get_address_metadata_from_root(root).ok_or("Invalid address")?;
-//             let address_owner = metadata.address.trim_start_matches("0x");
-//             let address_encoded = bs58::encode(address_owner).into_string();
-//             let timestamp = metadata.timestamp;
-
-//             // Game ID
-//             let input = payload.get("input").ok_or("Invalid field input")?;
-//             let game_id = input
-//                 .get("game_id")
-//                 .ok_or("Invalid field game_id")?
-//                 .as_str()
-//                 .ok_or("Invalid game_id")?;
-
-//             let mut manager = manager.lock().await;
-//             let table = manager.get_table(game_id)?;
-//             let table_id = table.get_id().to_owned();
-//             table.hit_player(&address_encoded, timestamp).await?;
-
-//             if !table.any_player_can_hit() {
-//                 manager.stop_game(&table_id).await?;
-//             }
-//         }
-//         Some("stand") => {
-//             let input = payload.get("input").ok_or("Invalid field input")?;
-
-//             // Parsing JSON
-//             let game_id = input
-//                 .get("game_id")
-//                 .ok_or("Invalid field game_id")?
-//                 .as_str()
-//                 .ok_or("Invalid game_id")?;
-
-//             let metadata = get_address_metadata_from_root(root).ok_or("Invalid address")?;
-//             let address_owner = metadata.address.trim_start_matches("0x");
-//             let address_encoded = bs58::encode(address_owner).into_string();
-
-//             let mut manager = manager.lock().await;
-//             let table = manager.get_table(game_id)?;
-
-//             let name = table.get_name_player(&address_encoded).unwrap();
-//             let table_id = table.get_id().to_owned();
-//             table.stand_player(&address_encoded, metadata.timestamp)?;
-
-//             if !table.any_player_can_hit() {
-//                 manager.stop_game(&table_id).await?;
-//             }
-//             println!("Stand: {} game_id {}", name, game_id);
-//         }
-//         _ => Err("Invalid action")?,
-//     }
-
-//     Ok(None)
-// }
 
 fn start_handle_action(
     manager: Arc<Mutex<Manager>>,
@@ -264,8 +34,8 @@ fn start_handle_action(
                         err
                     });
 
-                if let Ok(Some(value)) = value {
-                    let _ = sender_middleware.send(value).await.map_err(|err| {
+                if let Ok(Some(report)) = value {
+                    let _ = sender_middleware.send(report).await.map_err(|err| {
                         eprintln!("Send to middleware error: {}", err);
                         err
                     });
@@ -275,6 +45,7 @@ fn start_handle_action(
     })
 }
 
+// Read from rollup and send to middleware
 fn start_rollup(manager: Arc<Mutex<Manager>>, sender: Sender<Value>) {
     tokio::spawn(async move {
         loop {
@@ -285,6 +56,7 @@ fn start_rollup(manager: Arc<Mutex<Manager>>, sender: Sender<Value>) {
     });
 }
 
+// Send message to report
 fn listener_send_message_to_middleware(mut receiver: Receiver<Value>) {
     tokio::spawn(async move {
         while let Some(value) = receiver.recv().await {
--- dapp-contract-blackjack/src/rollups.rs
+++ dapp-contract-blackjack/src/rollups.rs
@@ -77,106 +77,11 @@ pub mod rollup {
 
         println!("body {:}", &body);
 
-        let result = handle_request_action(&body, manager, false).await?;
+        let result = handle_request_action(&body, manager, true).await?;
 
-        // sender.send(body).await?;
-        // handle_request_action(&body, manager, false).await?;
-
-        // let payload = get_payload_from_root(&body).ok_or("Invalid payload")?;
-        // let action = get_from_payload_action(&payload);
-
-        // println!("Action: {:}", action.as_deref().unwrap_or("None"));
-
-        // match action.as_deref() {
-        //     Some("show_player") => {
-        //         let input = payload.get("input").ok_or("Invalid field input")?;
-
-        //         // Parsing JSON
-        //         let address = input
-        //             .get("address")
-        //             .ok_or("Invalid field address")?
-        //             .as_str()
-        //             .ok_or("Invalid address")?;
-        //         let address_owner = address.trim_start_matches("0x");
-        //         let address_encoded = bs58::encode(address_owner).into_string();
-
-        //         let address_path = format!("./data/address/{}.json", address_encoded);
-        //         println!("Trying read {:}", &address_path);
-        //         let content = fs::read_to_string(address_path)?;
-        //         let json_as_hex = hex::encode(content.to_string());
-        //         let report = json!({ "payload": format!("0x{}", json_as_hex) });
-        //         println!("Report: {:}", report);
-        //         let _ = send_report(report.clone()).await;
-        //     }
-        //     Some("show_games") => {
-        //         let manager = manager.lock().await;
-        //         let games = manager.show_games_id_available();
-
-        //         let response = json!({
-        //             "games": games,
-        //         });
-        //         let json_as_hex = hex::encode(response.to_string());
-        //         let report = json!({ "payload": format!("0x{}", json_as_hex) });
-        //         println!("Report: {:}", report);
-        //         let _ = send_report(report.clone()).await;
-        //     }
-        //     Some("show_hands") => {
-        //         let input = payload.get("input").ok_or("Invalid field input")?;
-
-        //         // Parsing JSON
-        //         let game_id = input
-        //             .get("game_id")
-        //             .ok_or("Invalid field game_id")?
-        //             .as_str()
-        //             .ok_or("Invalid game_id")?;
-
-        //         let mut manager = manager.lock().await;
-
-        //         let table = manager.get_table(game_id)?;
-        //         let hands = table.generate_hands();
-        //         let json_as_hex = hex::encode(hands.to_string());
-        //         let report = json!({ "payload": format!("0x{}", json_as_hex) });
-
-        //         println!("Report: {:}", report);
-        //         let _ = send_report(report.clone()).await;
-        //     }
-        //     Some("show_winner") => {
-        //         let input = payload.get("input").ok_or("Invalid field input")?;
-
-        //         // Parsing JSON
-        //         let game_id = input
-        //             .get("game_id")
-        //             .ok_or("Invalid field game_id")?
-        //             .as_str()
-        //             .ok_or("Invalid game_id")?;
-
-        //         let table_id = input
-        //             .get("table_id")
-        //             .ok_or("Invalid field table_id")?
-        //             .as_str()
-        //             .ok_or("Invalid string table_id")?;
-
-        //         let manager = manager.lock().await;
-
-        //         println!(
-        //             "Finding score by table_id {} and game_id {} ...",
-        //             table_id, game_id
-        //         );
-        //         let scoreboard = manager
-        //             .get_scoreboard(table_id, game_id)
-        //             .ok_or("Scoreboard not found searching by table_id")?;
-
-        //         let response = json!({
-        //             "scoreboard": scoreboard.to_json(),
-        //         });
-        //         let json_as_hex = hex::encode(response.to_string());
-        //         let report = json!({ "payload": format!("0x{}", json_as_hex) });
-
-        //         println!("Report: {:}", report);
-        //         let _ = send_report(report.clone()).await;
-        //     }
-        //     _ => Err("Invalid inspect")?,
-        // };
+        if let Some(report) = result {
+            send_report(report).await?;
+        }
 
         Ok("accept")
     }
@@ -191,168 +96,16 @@ pub mod rollup {
 
         println!("body {:}", &body);
         let run_async = std::env::var("RUN_GAME_ASYNC").unwrap_or("true".to_string());
+
         if run_async == "true" {
             sender.send(body).await?;
             return Ok("accept");
         }
-        // let payload = get_payload_from_root(&body).ok_or("Invalid payload")?;
-        // let action = get_from_payload_action(&payload);
 
-        // match action.as_deref() {
-        //     Some("new_player") => {
-        //         let input = payload.get("input").ok_or("Invalid field input")?;
-        //         let player_name = check_fields_create_player(&input)?;
-
-        //         let encoded_name = bs58::encode(&player_name).into_string();
-
-        //         let metadata = get_address_metadata_from_root(&body).ok_or("Invalid address")?;
-        //         let address_owner = metadata.address.trim_start_matches("0x");
-        //         let address_encoded = bs58::encode(address_owner).into_string();
-
-        //         // Add player to manager
-        //         let player = Player::new(address_encoded.clone(), player_name.to_string());
-        //         let mut manager = manager.lock().await;
-        //         let player = Arc::new(player);
-        //         manager.add_player(player)?;
-
-        //         // Persist player
-        //         let need_write = true;
-        //         if need_write {
-        //             let address_owner_obj = json!({ "address": address_owner });
-        //             let address_path = format!("./data/address/{}.json", address_encoded);
-
-        //             write_json(&address_path, &address_owner_obj)
-        //                 .await
-        //                 .or(Err("Could not write address"))?;
-
-        //             let player_path = format!("./data/names/{}.json", encoded_name);
-        //             let player = json!({ "name": encoded_name });
-        //             write_json(&player_path, &player)
-        //                 .await
-        //                 .or(Err("Could not write player"))?;
-        //         }
-
-        //         let response = generate_message(json!({
-        //             "address": address_encoded,
-        //             "encoded_name": encoded_name,
-        //             "name": player_name,
-        //         }));
-
-        //         println!("Response: {:}", response);
-
-        //         return Ok("accept");
-        //     }
-        //     Some("join_game") => {
-        //         let input = payload.get("input").ok_or("Invalid field input")?;
-
-        //         // Address
-        //         let metadata = get_address_metadata_from_root(&body).ok_or("Invalid address")?;
-        //         let address_owner = metadata.address.trim_start_matches("0x");
-        //         let address_encoded = bs58::encode(address_owner).into_string();
-
-        //         let mut manager = manager.lock().await;
-        //         let player = manager.get_player_ref(&address_encoded)?;
-
-        //         // Parsing JSON
-        //         let game_id = input
-        //             .get("game_id")
-        //             .ok_or("Invalid field game_id")?
-        //             .as_str()
-        //             .ok_or("Invalid game_id")?;
-
-        //         manager.player_join(game_id, player.clone())?;
-        //         println!("Player joined: name {} game_id {}", player.name, game_id);
-        //     }
-
-        //     Some("start_game") => {
-        //         let input = payload.get("input").ok_or("Invalid field input")?;
-        //         let metadata = get_address_metadata_from_root(&body).ok_or("Invalid address")?;
-        //         // Parsing JSON
-        //         let game_id = input
-        //             .get("game_id")
-        //             .ok_or("Invalid field game_id")?
-        //             .as_str()
-        //             .ok_or("Invalid game_id")?;
-
-        //         let mut manager = manager.lock().await;
-
-        //         // Get game and make owner
-        //         let game = manager.drop_game(game_id)?;
-        //         // Generate table from game
-        //         let table = game.round_start(2, metadata.timestamp)?;
-        //         // Add table to manager
-        //         manager.add_table(table);
-        //         println!("Game started: game_id {}", game_id);
-        //     }
-
-        //     Some("stop_game") => {
-        //         let input = payload.get("input").ok_or("Invalid field input")?;
-
-        //         // Parsing JSON
-        //         let game_id = input
-        //             .get("game_id")
-        //             .ok_or("Invalid field game_id")?
-        //             .as_str()
-        //             .ok_or("Invalid game_id")?;
-
-        //         let mut manager = manager.lock().await;
-
-        //         manager.stop_game(game_id).await?;
-        //     }
-
-        //     Some("hit") => {
-        //         // Address
-        //         let metadata = get_address_metadata_from_root(&body).ok_or("Invalid address")?;
-        //         let address_owner = metadata.address.trim_start_matches("0x");
-        //         let address_encoded = bs58::encode(address_owner).into_string();
-        //         let timestamp = metadata.timestamp;
-
-        //         // Game ID
-        //         let input = payload.get("input").ok_or("Invalid field input")?;
-        //         let game_id = input
-        //             .get("game_id")
-        //             .ok_or("Invalid field game_id")?
-        //             .as_str()
-        //             .ok_or("Invalid game_id")?;
-
-        //         let mut manager = manager.lock().await;
-        //         let table = manager.get_table(game_id)?;
-        //         let table_id = table.get_id().to_owned();
-        //         table.hit_player(&address_encoded, timestamp).await?;
-
-        //         if !table.any_player_can_hit() {
-        //             manager.stop_game(&table_id).await?;
-        //         }
-        //     }
-
-        //     Some("stand") => {
-        //         let input = payload.get("input").ok_or("Invalid field input")?;
-
-        //         // Parsing JSON
-        //         let game_id = input
-        //             .get("game_id")
-        //             .ok_or("Invalid field game_id")?
-        //             .as_str()
-        //             .ok_or("Invalid game_id")?;
-
-        //         let metadata = get_address_metadata_from_root(&body).ok_or("Invalid address")?;
-        //         let address_owner = metadata.address.trim_start_matches("0x");
-        //         let address_encoded = bs58::encode(address_owner).into_string();
-
-        //         let mut manager = manager.lock().await;
-        //         let table = manager.get_table(game_id)?;
-
-        //         let name = table.get_name_player(&address_encoded).unwrap();
-        //         let table_id = table.get_id().to_owned();
-        //         table.stand_player(&address_encoded, metadata.timestamp)?;
-
-        //         if !table.any_player_can_hit() {
-        //             manager.stop_game(&table_id).await?;
-        //         }
-        //         println!("Stand: {} game_id {}", name, game_id);
-        //     }
-        //     _ => Err("Invalid action")?,
-        // }
+        let result = handle_request_action(&body, manager, true).await?;
+        if let Some(report) = result {
+            send_report(report).await?;
+        }
 
         Ok("accept")
     }
