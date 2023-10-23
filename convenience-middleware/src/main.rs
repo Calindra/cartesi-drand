9b8f4eb1dd9ed6f242a83477622b7f38bbf4fd88
Bruno Ochotorena <hackerhilt@gmail.com>
2023-10-23 16:30:36 -0300

    feat: more changes

--- convenience-middleware/src/main.rs
+++ convenience-middleware/src/main.rs
@@ -307,7 +307,7 @@ async fn main() -> std::io::Result<()> {
             .service(routes::consume_buffer)
             .service(routes::update_drand_config)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
