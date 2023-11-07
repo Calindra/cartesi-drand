fbef19494460f1900b0b4e83cd42aa4ceb6479d7
Bruno Ochotorena <hackerhilt@gmail.com>
2023-11-07 17:41:41 -0300

    feat: more changes

--- web3-client/src/main.ts
+++ web3-client/src/main.ts
@@ -1,7 +1,14 @@
-import { IInputBox__factory, InputBox, InputBox } from "@cartesi/rollups";
-import type { Signer, providers } from "ethers";
+import { IInputBox__factory, type InputBox } from "@cartesi/rollups";
+import {
+  ethers,
+  type Signer,
+  type Provider,
+  Wallet,
+  AddressLike,
+  resolveAddress,
+  ContractTransactionResponse,
+} from "ethers";
 type ObjectLike = Record<string, unknown>;
-type Provider = providers.Provider;
 
 export interface Log {
   info(...args: unknown[]): void;
@@ -9,16 +16,13 @@ export interface Log {
 }
 
 export interface CartesiContructor {
-  endpoint: URL;
-  address: string;
+  endpoint_cartesi_rollups: URL;
+  address_dapp: AddressLike;
   signer: Signer;
   wallet: Wallet;
   provider: Provider;
   logger: Log;
 }
-
-export class Wallet {}
-// export class Provider {}
 
 export class Utils {
   static isObject(value: unknown): value is ObjectLike {
@@ -46,13 +50,21 @@ export class CartesiClient {
   constructor(private readonly config: CartesiContructor) {}
 
   /**
-   * @param data The data to be sent to the Cartesi Machine, transform to payload
+   * Convert AddressLike, type used by ethers to string
    */
-  async inspect<T extends ObjectLike, U extends ObjectLike>(data: T): Promise<U | null> {
-    const inputJSON = JSON.stringify({ input: data });
+  private async getAddress(): Promise<string> {
+    return resolveAddress(this.config.address_dapp);
+  }
+
+  /**
+   * @param payload The data to be sent to the Cartesi Machine, transform to payload
+   * used to request reports
+   */
+  async inspect<T extends ObjectLike, U extends ObjectLike>(payload: T): Promise<U | null> {
+    const inputJSON = JSON.stringify({ input: payload });
     const jsonEncoded = encodeURIComponent(inputJSON);
 
-    const url = new URL(this.config.endpoint);
+    const url = new URL(this.config.endpoint_cartesi_rollups);
     url.pathname = url.pathname.replace(/\/$/, "");
     url.pathname += `/${jsonEncoded}`;
 
@@ -61,7 +73,7 @@ export class CartesiClient {
       const result: unknown = await response.json();
 
       if (Utils.isObject(result) && "reports" in result && Utils.isArrayNonNullable(result.reports)) {
-        const firstReport = result.reports[0];
+        const firstReport = result.reports.at(0);
 
         if (Utils.isObject(firstReport) && "payload" in firstReport && typeof firstReport.payload === "string") {
           const payload = Hex.hex2a(firstReport.payload.replace(/^0x/, ""));
@@ -77,16 +89,16 @@ export class CartesiClient {
 
   /**
    *
-   * @param data The data to be sent to the Cartesi Machine, transform to payload
+   * @param payload The data to be sent to the Cartesi Machine, transform to payload
    */
-  async advance<T extends ObjectLike>(data: T) {
+  async advance<T extends ObjectLike>(payload: T) {
     try {
       const { provider, logger, signer } = this.config;
-      const network = await provider.getNetwork();
+      const [address, network] = await Promise.all([this.getAddress(), provider.getNetwork()]);
       logger.info(`connected to chain ${network.chainId}`);
 
       // connect to rollups,
-      const inputContract = IInputBox__factory.connect(InputBox.address, signer);
+      const inputContract = IInputBox__factory.connect(address, signer);
       const signerAddress = await signer.getAddress();
       logger.info(`using account "${signerAddress}"`);
 
@@ -101,18 +113,11 @@ export class CartesiClient {
       );
 
       // send transaction
-      const dappAddress = DApp.address; // '0x142105FC8dA71191b3a13C738Ba0cF4BC33325e2'
-      const tx = <ContractTransactionResponse>await inputContract.addInput(dappAddress, inputBytes);
-      // const tx: any = await inputContract.addInput(dappAddress, inputBytes);
-      console.log(`transaction: ${tx.hash}`);
-      console.log("waiting for confirmation...");
+      const tx = <ContractTransactionResponse>await inputContract.addInput(address, inputBytes);
+      logger.info(`transaction: ${tx.hash}`);
+      logger.info("waiting for confirmation...");
       const receipt = await tx.wait(1);
-      console.log(JSON.stringify(receipt));
-      // find reference to notice from transaction receipt
-      // const inputKeys = getInputKeys(receipt);
-      // console.log(
-      //     `input ${inputKeys.input_index} added`
-      // );
+      logger.info(JSON.stringify(receipt));
     } catch (e) {
       console.error(e);
     }
