0b6518c7602730921cd094d2270bda28aa974d4c
sandhilt <6170125+sandhilt@users.noreply.github.com>
2025-05-15 11:09:47 -0300

    chore: add Nix flake configuration and environment setup

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Development environment for the project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs_20
+            pkgs.rustup
+            pkgs.libclang
+          ];
+        };
+      }
+    );
+}
